830bf127a5aa7ca0cad57cb7f82c2ce299289aed
MrLarkyy_ <48769171+MrLarkyy@users.noreply.github.com>
2023-02-04 21:21:29 +0100

    Prepared the MySQLAdapter

---
+++ src/main/java/xyz/larkyy/aquaticlibrary/database/configuration/MySQLAdapter.java
@@ -0,0 +1,12 @@
+package xyz.larkyy.aquaticlibrary.database.configuration;
+
+import xyz.larkyy.aquaticlibrary.database.driver.SQLDriver;
+
+import java.io.IOException;
+
+public class MySQLAdapter extends SQLAdapter{
+    @Override
+    public SQLDriver setup() throws IOException, ClassNotFoundException {
+        return null;
+    }
+}
