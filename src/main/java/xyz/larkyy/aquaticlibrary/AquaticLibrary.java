992bd937523cb1cc7e6afecb7d3fd06ccafdeb8c
MrLarkyy_ <48769171+MrLarkyy@users.noreply.github.com>
2023-02-05 19:37:27 +0100

    Added comments to the BlockManager2.java

--- src/main/java/xyz/larkyy/aquaticlibrary/AquaticLibrary.java
+++ src/main/java/xyz/larkyy/aquaticlibrary/AquaticLibrary.java
@@ -5,6 +5,7 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.larkyy.aquaticlibrary.item.CustomItem;
 import xyz.larkyy.aquaticlibrary.item.CustomItemHandler;
+import xyz.larkyy.aquaticlibrary.prop.PropManager2;
 import xyz.larkyy.aquaticlibrary.service.ServiceManager;
 
 public class AquaticLibrary {
@@ -16,6 +17,7 @@ public class AquaticLibrary {
     public static void init(JavaPlugin plugin) {
         ServiceManager.addService(JavaPlugin.class,plugin);
         ServiceManager.addService(new CustomItemHandler());
+        ServiceManager.addService(new PropManager2());
 
         var item = CustomItem.create(Material.STONE);
         item.addPersistentData(PersistentDataType.INTEGER,1);
--- src/main/java/xyz/larkyy/aquaticlibrary/block/BlockManager2.java
+++ src/main/java/xyz/larkyy/aquaticlibrary/block/BlockManager2.java
@@ -22,11 +22,23 @@ public class BlockManager2 {
         this.sqlAdapter = sqlAdapter;
     }
 
+    /**
+     * Gets CustomBlock by location
+     *
+     * @param location  Your target location from which you want to get the CustomBlock instance
+     * @return          Instance of CustomBlock or null when no CustomBlock was found
+     */
     public CustomBlock getCustomBlock(Location location) {
         location = location.getBlock().getLocation();
         return customBlocks.get(location);
     }
 
+    /**
+     * Gets CustomBlock by instance of Block
+     *
+     * @param block Your target block from which you want to get the CustomBlock instance
+     * @return      Instance of CustomBlock or null when no CustomBlock was found
+     */
     public CustomBlock getCustomBlock(Block block) {
         var location = block.getLocation();
         return getCustomBlock(location);
@@ -36,14 +48,13 @@ public class BlockManager2 {
         return ServiceManager.getService(PropManager2.class);
     }
 
-    public <T extends CustomBlock> void registerBlock(T customBlock) {
-        var context = getManager().getContext(customBlock.getClass());
-        if (context == null) {
-            return;
-        }
-        getManager().addProp(customBlock);
-        context.addProp(customBlock);
-    }
+    /**
+     * Registers the block factory. It is required to register the block factory before registering
+     * the actual custom blocks!
+     *
+     * @param blockClass Class of your CustomBlock
+     * @param factory factory class of your CustomBlock
+     */
     public <T extends CustomBlock> void addFactory(Class<T> blockClass, PropFactory factory) throws Exception {
         if (contexts.containsKey(blockClass)) {
             return;
@@ -56,6 +67,23 @@ public class BlockManager2 {
         contexts.put(blockClass,context);
     }
 
+    /**
+     * Adds the CustomBlock to database and cache
+     * @param customBlock an instance of CustomBlock that you want to be registered
+     */
+    public <T extends CustomBlock> void registerBlock(T customBlock) {
+        var context = getManager().getContext(customBlock.getClass());
+        if (context == null) {
+            return;
+        }
+        getManager().addProp(customBlock);
+        context.addProp(customBlock);
+    }
+
+    /**
+     * Removes the CustomBlock from database and cache
+     * @param customBlock an instance of CustomBlock that you want to be unregistered
+     */
     public <T extends CustomBlock> void unregisterBlock(T customBlock) {
         var context = getManager().getContext(customBlock.getClass());
         if (context == null) {
@@ -65,6 +93,10 @@ public class BlockManager2 {
         context.deleteProp(customBlock);
     }
 
+    /**
+     * Adds the CustomBlock to cache
+     * @param customBlock an instance of CustomBlock that you want to be added
+     */
     public <T extends CustomBlock> void addCustomBlock(T customBlock) {
         customBlocks.put(customBlock.getBlock().getLocation(),customBlock);
         if (contexts.containsKey(customBlock.getClass())) {
@@ -72,10 +104,24 @@ public class BlockManager2 {
         }
     }
 
+    /**
+     * Removes the CustomBlock from cache
+     * @param customBlock an instance of CustomBlock that you want to be removed
+     */
     public <T extends CustomBlock> void removeCustomBlock(T customBlock) {
         customBlocks.remove(customBlock.getBlock().getLocation());
         if (contexts.containsKey(customBlock.getClass())) {
             getManager().removeProp(customBlock);
         }
+    }
+
+    /**
+     * Initializes the class and registers it in the Service Manager
+     * @param   adapter the sql adapter for context
+     */
+    public static BlockManager2 init(SQLAdapter adapter) {
+        var manager = new BlockManager2(adapter);
+        ServiceManager.addService(manager);
+        return manager;
     }
 }
